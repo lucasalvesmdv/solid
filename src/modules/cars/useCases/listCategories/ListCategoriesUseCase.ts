1636ff84b1237c14b50c8738de5d3713629de468
Lucas Alves <60105209+lucasalvesmdv@users.noreply.github.com>
2021-11-23 23:17:44 -0300

    inicio

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,19 @@
+import { Category } from '../../model/Category';
+import { ICategoryRepository } from '../../repositories/ICategoriesRepository';
+
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoryRepository) {
+
+  }
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+
+  }
+
+
+}
+
+export {ListCategoriesUseCase}
